57860a3c577ef222f3f1f1402fcb9e8b7fcee186
paulacubero <paulacubero.e@gmail.com>
2024-01-21 17:37:32 +0100

    añadir cv-header

---
+++ src/components/cv-header/cv-header.jsx
@@ -0,0 +1,29 @@
+import { Logo } from '../logo/logo';
+import style from './style.module.css';
+
+export function CVHeader() {
+	return (
+		<>
+			<div className={style.presentationWrapper}>
+				<div className={style.logoWrapper}>
+					<Logo />
+				</div>
+				<div className={style.mainInfoWrapper}>
+					<div className={style.title}>
+						<h1>Paula Cubero</h1>
+						<div className={style.buttonWrapper}>
+							<button className={style.cvButton}>ES</button>
+							<button className={style.cvButtonLuz}></button>
+						</div>
+					</div>
+					<div>
+						<h3>Técnico superior de desarrollo de aplicaciones web.</h3>
+					</div>
+					<div className={style.buttonWrapper}>
+						<button className={style.cvButton}>CV</button>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
